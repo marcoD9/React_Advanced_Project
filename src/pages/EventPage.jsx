dfb4cd2ba047e20dbf99c64e4fc46d90a3c14f1b
Marco <marco.dmail96@gmail.com>
2024-12-16 08:29:16 +0100

    Delete EventDetails and EventsList components and replace them with routes

--- src/pages/EventPage.jsx
+++ src/pages/EventPage.jsx
@@ -1,6 +1,70 @@
-import React from 'react';
-import { Heading } from '@chakra-ui/react';
+import {
+  CardBody,
+  Heading,
+  Image,
+  Text,
+  Card,
+  List,
+  ListItem,
+} from "@chakra-ui/react";
+import { useLoaderData } from "react-router-dom";
+
+async function fetchCategories(ids) {
+  const promises = ids.map(async (id) => {
+    const response = await fetch(`http://localhost:3000/categories/${id}`);
+    return await response.json();
+  });
+
+  return await Promise.all(promises);
+}
+
+export const loader = async ({ params }) => {
+  try {
+    const response = await fetch(
+      `http://localhost:3000/events/${params.eventId}`
+    );
+    if (!response.ok) {
+      throw new Error("Event not found");
+    }
+    const event = await response.json();
+
+    const categories = await fetchCategories(event.categoryIds); // Call the new function
+
+    return { event, categories };
+  } catch (error) {
+    console.error("Error fetching event:", error);
+  }
+};
 
 export const EventPage = () => {
-  return <Heading>Event</Heading>;
+  const { event, categories } = useLoaderData();
+  console.log(categories);
+  return (
+    <>
+      <Heading>Event</Heading>
+      <Card key={event.title}>
+        <CardBody m={16}>
+          <div className="event">
+            <Image src={event.image} alt={event.title} />
+            <Heading>{event.title}</Heading>
+            <Text>{event.description}</Text>
+            <Text>Location: {event.location}</Text>
+            <Text>
+              From: {event.startTime} To: {event.endTime}
+            </Text>
+            {categories.length > 0 && (
+              <>
+                <Text>Categories: </Text>
+                <List key={event.id}>
+                  {categories.map((category) => (
+                    <ListItem key={category.id}>{category.name}</ListItem>
+                  ))}
+                </List>
+              </>
+            )}
+          </div>
+        </CardBody>
+      </Card>
+    </>
+  );
 };
--- src/pages/EventsPage.jsx
+++ src/pages/EventsPage.jsx
@@ -1,13 +1,29 @@
-import React, { useState } from "react";
-import { Heading } from "@chakra-ui/react";
-import { useEffect } from "react";
-import { EventsList } from "../components/EventsList";
+import { CardBody, Heading, Image, Card } from "@chakra-ui/react";
+import { useLoaderData, Link } from "react-router-dom";
+
+export const loader = async () => {
+  const events = await fetch("http://localhost:3000/events");
+  return { events: await events.json() };
+};
 
 export const EventsPage = () => {
+  const { events } = useLoaderData();
+
   return (
-    <>
+    <div className="events-list">
       <Heading>List of events</Heading>
-      <EventsList />
-    </>
+      {events.map((event) => (
+        <Card key={event.id}>
+          <CardBody m={16}>
+            <div className="event">
+              <Link to={`event/${event.id}`}>
+                <Image src={event.image} alt={event.title} />
+                <Heading>{event.title}</Heading>
+              </Link>
+            </div>
+          </CardBody>
+        </Card>
+      ))}
+    </div>
   );
 };
