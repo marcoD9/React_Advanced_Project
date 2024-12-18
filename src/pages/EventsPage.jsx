39d575adeb2aa4f6897bdc3c13e827ea70c4807b
Marco <marco.dmail96@gmail.com>
2024-12-18 20:03:56 +0100

    Update EventsPage, add a filter to show events by category

--- src/pages/EventsPage.jsx
+++ src/pages/EventsPage.jsx
@@ -4,11 +4,13 @@ import {
   Image,
   Card,
   Box,
+  Select,
   Stack,
   Text,
+  Flex,
   Input,
 } from "@chakra-ui/react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useLoaderData, Link } from "react-router-dom";
 
 export const loader = async () => {
@@ -19,38 +21,84 @@ export const loader = async () => {
 export const EventsPage = () => {
   const { events } = useLoaderData();
   const [searchField, setSearchField] = useState("");
+  const [selectedCategory, setSelectedCategory] = useState("");
+  const [filteredEvents, setFilteredEvents] = useState([]); // Combined filtered events
 
-  //filter events to match the title with the input field
-  const matchedEvent = events.filter((event) => {
-    return event.title.toLowerCase().includes(searchField.toLowerCase());
-  });
+  // Filter based on search field
+  useEffect(() => {
+    const filteredByName = events.filter((event) =>
+      event.title.toLowerCase().includes(searchField.toLowerCase())
+    );
+    setFilteredEvents(filteredByName);
+  }, [searchField, events]); //executed on search field change
 
+  // Filter based on category
+  useEffect(() => {
+    const filteredByCategory = events.filter((event) => {
+      if (selectedCategory == "") {
+        return true; // No category filter applied
+      }
+      return event.categoryIds.includes(parseInt(selectedCategory));
+    });
+    setFilteredEvents(filteredByCategory);
+  }, [selectedCategory, events]); //executed on category change
+
+  // Handle change in the text input
   const handleChange = (event) => {
     setSearchField(event.target.value);
   };
 
+  // Handle change in the category select
+  const handleCategoryChange = (event) => {
+    setSelectedCategory(Number(event.target.value));
+  };
+
   return (
-    <Box w="30vw">
-      <Stack>
-        <Text fontWeight="bolder">Search events by name:</Text>
-        <Text fontWeight="light">{searchField}</Text>
-        <Input onChange={handleChange} />
+    <>
+      <Stack alignItems="center" textAlign="center">
+        <Flex flexDirection="column">
+          <Text fontWeight="bolder">Search events by name:</Text>
+          <Text fontWeight="light">{searchField}</Text>
+          <Input w="40vw" onChange={handleChange} />
+        </Flex>
+        <Flex flexDirection="column">
+          <Text>Category:</Text>
+          <Select
+            placeholder="Choose a category"
+            onChange={handleCategoryChange}
+          >
+            <option value="">All</option>
+            <option value="1">Sports</option>
+            <option value="2">Games</option>
+            <option value="3">Relaxation</option>
+          </Select>
+        </Flex>
       </Stack>
-      <div className="events-list">
-        <Heading>List of events</Heading>
-        {matchedEvent.map((event) => (
-          <Card key={event.id}>
-            <CardBody m={16}>
-              <div className="event">
-                <Link to={`event/${event.id}`}>
-                  <Image src={event.image} alt={event.title} />
-                  <Heading>{event.title}</Heading>
-                </Link>
-              </div>
-            </CardBody>
-          </Card>
-        ))}
-      </div>
-    </Box>
+      <Stack alignItems="center">
+        <Flex
+          maxH="max-content"
+          maxWidth="max-content"
+          justifyContent="center"
+          gap="8"
+          wrap="wrap"
+        >
+          <div className="events-list">
+            <Heading>List of events</Heading>
+            {filteredEvents.map((event) => (
+              <Card key={event.id}>
+                <CardBody m={16}>
+                  <div className="event">
+                    <Link to={`event/${event.id}`}>
+                      <Image src={event.image} alt={event.title} />
+                      <Heading>{event.title}</Heading>
+                    </Link>
+                  </div>
+                </CardBody>
+              </Card>
+            ))}
+          </div>
+        </Flex>
+      </Stack>
+    </>
   );
 };
