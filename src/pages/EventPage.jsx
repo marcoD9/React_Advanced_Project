27da9a99717c3823f9e09591fef73188dc0a7693
Marco <marco.dmail96@gmail.com>
2024-12-20 16:33:21 +0100

    Update EventPage to fetch and render details about the user

--- src/pages/EventPage.jsx
+++ src/pages/EventPage.jsx
@@ -4,21 +4,30 @@ import {
   Image,
   Text,
   Card,
+  Stack,
   List,
   ListItem,
 } from "@chakra-ui/react";
 import { useLoaderData } from "react-router-dom";
 
-async function fetchCategories(ids) {
-  const promises = ids.map(async (id) => {
-    const response = await fetch(`http://localhost:3000/categories/${id}`);
-    return await response.json();
-  });
-
-  return await Promise.all(promises);
-}
-
 export const loader = async ({ params }) => {
+  //-------------------------------------------------------------------------------//
+  //Fetch categories datas based on the event's categoryIds
+  async function fetchCategories(ids) {
+    const promises = ids.map(async (id) => {
+      const response = await fetch(`http://localhost:3000/categories/${id}`);
+      return await response.json();
+    });
+    return await Promise.all(promises);
+  }
+  //-------------------------------------------------------------------------------//
+  //Fetch users datas based on the event's createdBy property
+  async function fetchUsersId(id) {
+    const response = await fetch(`http://localhost:3000/users/${id}`);
+    return await response.json();
+  }
+  //-------------------------------------------------------------------------------//
+
   try {
     const response = await fetch(
       `http://localhost:3000/events/${params.eventId}`
@@ -26,26 +35,32 @@ export const loader = async ({ params }) => {
     if (!response.ok) {
       throw new Error("Event not found");
     }
+
     const event = await response.json();
+    const categories = await fetchCategories(event.categoryIds);
+    const user = await fetchUsersId(event.createdBy);
 
-    const categories = await fetchCategories(event.categoryIds); // Call the new function
-
-    return { event, categories };
+    return { event, user, categories };
   } catch (error) {
     console.error("Error fetching event:", error);
   }
 };
 
 export const EventPage = () => {
-  const { event, categories } = useLoaderData();
+  const { event, user, categories } = useLoaderData();
   console.log(categories);
   return (
     <>
       <Heading>Event</Heading>
       <Card key={event.title}>
         <CardBody m={16}>
-          <div className="event">
-            <Image src={event.image} alt={event.title} />
+          <Stack>
+            <Image
+              boxSize="400px"
+              objectFit="cover"
+              src={event.image}
+              alt={event.title}
+            />
             <Heading>{event.title}</Heading>
             <Text>{event.description}</Text>
             <Text>Location: {event.location}</Text>
@@ -62,7 +77,18 @@ export const EventPage = () => {
                 </List>
               </>
             )}
-          </div>
+          </Stack>
+          <Stack>
+            <Text>Created by: </Text>
+            <Image
+              borderRadius="100"
+              w="10%"
+              h="10%"
+              src={user.image}
+              alt={`Profile picture of the user: ${user.name}`}
+            />
+            <Text>{user.name}</Text>
+          </Stack>
         </CardBody>
       </Card>
     </>
